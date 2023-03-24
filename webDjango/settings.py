6e65f52dadabbda5506f8dd06ca1d845c86cb476
Admin <khanhtruong326@gmail.com>
2023-03-24 11:09:49 +0700

    'fourth'

--- webDjango/settings.py
+++ webDjango/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rrmzk1u262n2ku(y1^wrazcrssthf*!6zgl%$nkxu^-vovyx!c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
